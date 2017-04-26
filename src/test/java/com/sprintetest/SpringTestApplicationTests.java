521cd80a8062140c25e43239d36db1630f010124
MarkLewandowski <marek.levandovski@gmail.com>
2017-04-26 22:21:00 +0200

    Add spring test for printFullName method

--- src/test/java/com/sprintetest/SpringTestApplicationTests.java
+++ src/test/java/com/sprintetest/SpringTestApplicationTests.java
@@ -1,16 +1,26 @@
 package com.sprintetest;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringTestApplicationTests {
 
+	@Autowired
+	private PrintController printController;
+
 	@Test
 	public void contextLoads() {
-	}
+		String fullName = printController.printFullName();
 
+		Assert.assertThat(fullName, is("MarekLewandowski"));
+	}
 }
