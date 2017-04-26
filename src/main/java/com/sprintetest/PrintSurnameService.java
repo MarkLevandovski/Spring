7bb2eede35059e2afc4f0e4fa47439eaa0fb131c
MarkLewandowski <marek.levandovski@gmail.com>
2017-04-26 22:16:26 +0200

    Add PrintSurnameService.java

---
+++ src/main/java/com/sprintetest/PrintSurnameService.java
@@ -0,0 +1,15 @@
+package com.sprintetest;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Marek Lewandowski on 26.04.17.
+ */
+@Service
+public class PrintSurnameService {
+
+    public String printSurname() {
+
+        return "Lewandowski";
+    }
+}
