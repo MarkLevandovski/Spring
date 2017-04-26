3ecaefd7a97ffcd3ac472c067a228b1481361299
MarkLewandowski <marek.levandovski@gmail.com>
2017-04-26 22:35:11 +0200

    Add PrintControllerJunitTest.java

---
+++ src/test/java/com/sprintetest/PrintControllerJunitTest.java
@@ -0,0 +1,30 @@
+package com.sprintetest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Marek Lewandowski on 26.04.17.
+ */
+public class PrintControllerJunitTest {
+    private PrintNameService printNameService;
+    private PrintSurnameService printSurnameService;
+    private PrintController printController;
+
+    @Before
+    public void setUp() {
+        printNameService = new PrintNameService();
+        printSurnameService = new PrintSurnameService();
+        printController = new PrintController();
+    }
+
+    @Test
+    public void printFullNameTest() {
+        printNameService.printName();
+        printSurnameService.printSurname();
+        String fullName = printController.printFullName();
+
+        Assert.assertEquals(fullName, "MarekLewandowski");
+    }
+}
